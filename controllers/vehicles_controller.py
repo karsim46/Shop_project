69a65be3390d4c4ebab175eddf646c555279b8f0
Simon <simonwatson176@gmail.com>
2021-07-12 13:08:18 +0100

    vehicles route added to controller

--- controllers/vehicles_controller.py
+++ controllers/vehicles_controller.py
@@ -6,3 +6,7 @@ import repositories.manufacturer_repository as manufacturer_repository
 
 vehicles_blueprint = Blueprint("vehicles", __name__)
 
+@vehicles_blueprint.route("/vehicles")
+def vehicles():
+    vehicles = vehicle_repository.select_all() 
+    return render_template("vehicles/index.html", all_vehicles = vehicles)
