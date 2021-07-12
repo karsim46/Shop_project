69af27e2b4e280da3e1c493d71be1b6dccdf0f83
Simon <simonwatson176@gmail.com>
2021-07-12 14:53:49 +0100

    2 x vehicle manufacturers saved to repo

--- console.py
+++ console.py
@@ -1,1 +1,16 @@
 import pdb
+from models.vehicle import Vehicle
+from models.manufacturer import Manufacturer
+
+import repositories.vehicle_repository as vehicle_repository
+import repositories.manufacturer_repository as manufacturer_repository
+
+vehicle_repository.delete_all()
+manufacturer_repository.delete_all()
+
+vehicle1 = Manufacturer("Delorean")
+manufacturer_repository.save(vehicle1)
+
+vehicle2 = Manufacturer("Dodge")
+manufacturer_repository.save(vehicle2)
+
