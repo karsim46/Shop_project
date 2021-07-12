93098451563b847e1bbee559b6112c2eb0e3ad78
Simon <simonwatson176@gmail.com>
2021-07-12 13:51:28 +0100

    being able to seelect one vehicle from inventory

--- repositories/vehicle_repository.py
+++ repositories/vehicle_repository.py
@@ -4,6 +4,7 @@ from models.vehicle import Vehicle
 from models.manufacturer import Manufacturer
 import repositories.manufacturer_repository as manufacturer_repository
 
+# save feature for being able to add a  vehicle to repo.
 def save(vehicle):
     sql = "INSERT INTO vehicles (description, engine, gearbox, colour, price, year, quantity, for_sale, manufacturer_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING *"
     values = [vehicle.description, vehicle.engine, vehicle.gearbox, vehicle.colour, vehicle.price, vehicle.year, vehicle.quantity, vehicle.for_sale, vehicle.manufacturer_id]
@@ -11,7 +12,7 @@ def save(vehicle):
     id = results[0]['id']
     vehicle.id = id
     return vehicle
-
+# select all feature displaying all inventory.
 def select_all():
     vehicles = []
 
@@ -23,4 +24,16 @@ def select_all():
         vehicle = Vehicle(row['description'], row['engine'], row['gearbox'], row['colour'], row['price'], row['year'], row['quantity'], row['for_sale'], manufacturer, row['id'])
         vehicles.append(vehicle)
     return vehicles
+
+# select one feature allowing user to select one vehicle by id(determined by database).
+def select(id):
+    vehicle = None
+    sql = "SELECT * FROM vehicles WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        manufacturer = manufacturer_repository.select(result['manufacturer_id'])
+        vehicle = Vehicle(result['description'], result['engine'], result['gearbox'], result['colour'], result['price'], result['year'], result['quantity'], result['for_sale'], manufacturer, result['id'])
+        return vehicle
 
