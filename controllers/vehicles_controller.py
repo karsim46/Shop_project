fc7b8d6c4bb5e83cd0186bf3e9897a6f8e373fdb
Simon <simonwatson176@gmail.com>
2021-07-12 18:43:41 +0100

    update vehicle controller created

--- controllers/vehicles_controller.py
+++ controllers/vehicles_controller.py
@@ -45,11 +45,33 @@ def show_vehicle(id):
 
 # EDIT
 # GET '/vehicles/<id>/edit'
-@vehicles_blueprint("/vehicles/<id>/edit", methods=['GET'])
+@vehicles_blueprint.route("/vehicles/<id>/edit", methods=['GET'])
 def edit_vehicle(id):
     vehicle = vehicle_repository.select(id)
     manufacturers = manufacturer_repository.select_all()
     return render_template('vehicles/edit.html', vehicle = vehicle, all_manufacturers = manufacturers)
+
+# UPDATE
+# PUT '/vehicles/<id>'
+@vehicles_blueprint.route("/vehicles/<id>", methods=['POST'])
+def update_vehicle(id):
+    description = request.form['description']
+    engine = request.form['engine']
+    gearbox = request.form['gearbox']
+    colour = request.form['colour']
+    price = request.form['price']
+    year = request.form['year']
+    quantity = request.form['quantity']
+    for_sale = request.form['for_sale']
+    manufacturer = manufacturer_repository.select(request.form['manufacturer.id'])
+    vehicle = Vehicle(description, engine, gearbox, colour, price, year, quantity, for_sale, manufacturer)
+    #req help relating to class
+    print(vehicle.manufacturer.display_name)
+    vehicle_repository.update(vehicle)
+    return redirect('/vehicles')
+
+
+
 
 
 
