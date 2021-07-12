4f37ed5a9acf268aa4d5b0e8647e2e1783da55ad
Simon <simonwatson176@gmail.com>
2021-07-12 15:52:25 +0100

    create vehicles function for website

--- controllers/vehicles_controller.py
+++ controllers/vehicles_controller.py
@@ -11,3 +11,19 @@ def vehicles():
     vehicles = vehicle_repository.select_all() 
     return render_template("vehicles/index.html", all_vehicles = vehicles)
 
+#create 
+#POST '/vehicles'
+@vehicles_blueprint.route("/vehicles", methods =['POST'])
+def create_vehicles():
+    description = request.form['description']
+    engine = request.form['engine']
+    gearbox = request.form['gearbox']
+    colour = request.form['colour']
+    price = request.form['price']
+    year = request.form['year']
+    quantity = request.form['quantity']
+    for_sale = request.form['for_sale']
+    manufacturer = manufacturer_repository.select(request.form['manufacturer.id'])
+    vehicle = Vehicle(description, engine, gearbox, colour, price, year, quantity, for_sale, manufacturer)
+    vehicle_repository.save(vehicle)
+    return redirect('/vehicles')
