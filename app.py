df50eda647a55773387bf6f627aea46738c749a6
Simon <simonwatson176@gmail.com>
2021-07-12 10:57:40 +0100

    app.py populated

--- app.py
+++ app.py
@@ -4,3 +4,12 @@ from controllers.vehicles_controller import vehicles_blueprint
 
 app = Flask(__name__)
 
+app.register_blueprint(vehicles_blueprint)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
