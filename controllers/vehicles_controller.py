4818f0482a36ffd1267ffdde46b6f4f7221e08df
Simon <simonwatson176@gmail.com>
2021-07-12 11:04:33 +0100

    controller started

---
+++ controllers/vehicles_controller.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+from models.vehicle import Vehicle
+import repositories.vehicle_repository as vehicle_repository
+import repositories.manufacturer_repository as manufacturer_repository
+
+vehicles_blueprint = Blueprint("vehicles," __name__)
+
