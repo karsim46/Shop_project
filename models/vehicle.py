3e767fafe79e104148af94d694ae6971527cef32
Simon <simonwatson176@gmail.com>
2021-07-12 11:30:13 +0100

    Vehicle class created

---
+++ models/vehicle.py
@@ -0,0 +1,12 @@
+class Vehicle:
+
+    def __init__(self, description, engine, gearbox, colour, price, year, quantity, for_sale,id = None, ):
+        self.description = description
+        self.engine = engine
+        self.gearbox = gearbox
+        self.colour = colour
+        self.price = price
+        self.year = year
+        self.quantity = quantity
+        self.for_sale = for_sale
+        self.id = id
